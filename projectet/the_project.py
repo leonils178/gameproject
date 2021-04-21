9bce992b9a4fa8aff05eea3ce8b8ed04ca1de375
Leo Nilsson (EDU) <ln7322@ad.lystkom.se>
2021-04-21 15:42:34 +0200

    jobbade med fel i lopen som jag hittat men är inte riktigt klar

--- projectet/the_project.py
+++ projectet/the_project.py
@@ -1,11 +1,10 @@
-print("Welcome to hangman")
-word=[sun]
+print("Welcome to hangman") #a small menu
 print("In this game you will have to guess a word for which you have 10 attempts before the game is lost")
-x1=("Do you wish to play hangman, press y to continue, or n to exit the game ") 
-x1=x1.upper
+x1=input("Do you wish to play hangman, press y to continue, or n to exit the game ") 
+x1=x1.upper()
 letter=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-
+'''
 if x1==('Y'):
     x2=10
     word=["S","U","N"]
@@ -13,27 +12,38 @@ if x1==('Y'):
     correct=[]
     
     while x2>=0:
-        guess=input("Guess a letter please")
+        index=0
+        guess=input("Guess a letter please ")
         while len(guess)>1:
-            guess=input("Please only enter one letter at a time as your guess")
-            while g>0:
-                guess=guess.upper
-                for each l in word:
-                    if l==guess:
-                        return guess
-                        
+            guess=input("Please only enter one letter at a time as your guess ")
+        while g>=len(word):
+            guess=guess.upper()
+            for x3 in word:
+                index=index+1
+                if x3==guess:
+                    word.pop(index-1)
+                    correct.append(guess)
+                    index=0
+'''
                 
-            
-        index=0#look out for bugs here
-        for x3 in word:
-            index=index+1
-            if guess==x3:
-                
-                print("Correct guess, you have",len(word),"letters left to guess")
-                index=0
-                
-        else:
-            x2=x2-1
-            print("Wrong guess, you have",x2,"attempts left to guess the correct letters")
-            index=0
-        
+if x1==('Y'):
+    x2=10
+    word=["S","U","N"]
+    g=len(word)
+    correct=[]
+    
+    
+    while g>=len(word):
+        index=0
+        guess=input("Guess a letter please ")
+        while len(guess)>1:
+            guess=input("Please only enter one letter at a time as your guess ")
+        while x2>=10:
+            guess=guess.upper()
+            for x3 in word:
+                index=index+1
+                if x3==guess:
+                    word.pop(index-1)
+                    correct.append(guess)
+                    index=0
+
