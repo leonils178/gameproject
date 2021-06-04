5a915f0ae04a6c5848fa90508208633732a688ce
Leo Nilsson (EDU) <ln7322@ad.lystkom.se>
2021-06-04 11:02:46 +0200

    the project is now in an almost optimal state with really only small ease of life things left to fix such as not accepting swedish letters

--- projectet/the_project.py
+++ projectet/the_project.py
@@ -1,27 +1,44 @@
 # coding: utf-8
+import random
+def wordGenerator(): #the wordgenerator of the program as a function
+    randomIndex=random.randint(0,9)
+    word1=["H","A","M","B","U","R","G","E","R"]
+    word2=["U","N","I","C","O","R","N"]
+    word3=["A","U","T","O","M","O","B","I","L","E"]
+    word4=["C","O","M","P","U","T","E","R"]
+    word5=["R","I","V","E","R"]
+    word6=["C","H","A","I","R"]
+    word7=["C","E","I","L","I","N","G"]
+    word8=["M","E","G","A","L","O","M","A","N","I","A","C"]
+    word9=["B","O","A","T"]
+    word10=["P","E","N","C","I","L"]
+    list=[word1,word2,word3,word4,word5,word6,word7,word8,word9,word10]
+    return list[randomIndex]
+    
+    
 print("Welcome to hangman") #a small menu
 print("In this game you will have to guess a word for which you have 10 attempts before the game is lost") #rulesblock
-x1=input("Do you wish to play hangman, press y to continue, or n to exit the game: ") # menu 2 so to speak 
-x1=x1.upper()
+theChoise=input("Do you wish to play hangman, press y to continue, or n to exit the game: ") # menu 2 so to speak 
+theChoise=theChoise.upper()
 letters=["1","2","3","4","5","6","7","8","9","0","Å","Ä","Ö"] 
 guessedLetters=[]
-correctguesses=[]
+correctGuesses=[]
 
 
-if x1==('Y'):
-    x2=10 #the amount of attempts
-    word=["S","U","N","S","H","I","N","E"] 
-    g=len(word)#making it so that the function works with other words if i ever add a random word generator
-    word2= [] #here we put both the undershlashes and the correct letters
+if theChoise==('Y'):
+    lifes=10 #the amount of attempts
+    word=wordGenerator()    
+    guessedWord= [] #here we put both the undershlashes and the correct letters
     for i in word:
-        word2.append("_") # here goes the part that is the small dashes under so that one can see what letters are correctly guessed
-    print ( " ".join(word2) ) # part 2 of the "underslashes"
-    index2=0
+        guessedWord.append("_") # here goes the part that is the small dashes under so that one can see what letters are correctly guessed
+    print ( " ".join(guessedWord) ) # part 2 of the "underslashes"
     
     
-    while word2!=word:
-        if x2==0:
+    
+    while guessedWord!=word:
+        if lifes==0:
             print("seems like you're out of guesses, better luck next time!") #here you end up if you guess too many wrongs
+            print(" ".join(word))
             print("exiting...")
             print("exiting...")
             print("exiting...")
@@ -31,24 +48,23 @@ if x1==('Y'):
         index=0
         guess=input("Guess a letter please: ") #guessing screen
         guess=guess.upper()
-        correctguesses=[]
+        correctGuesses=[]
         while len(guess)>1:
             guess=input("Please only enter one letter at a time as your guess: ") #only one letter
             guess=guess.upper()
-        for x5 in guessedLetters:
-            if x5==guess:
+        for failSafe1 in guessedLetters:
+            if failSafe1==guess:
                 guess=input("You have already tried that letter, try again: ") # here you come if you guess a guessed letter
                 guess=guess.upper()
-        for x6 in letters:
-            if x6==guess:
-                guess=input("that is not a valid letter, try again:") # in case of retarded guesses, does not work with shifted letters or altgr.
+        while guess.isalpha()==False:
+                guess=input("that is not a valid letter, try again: ") # in case of retarded guesses, does not work with shifted letters or altgr.
         guessedLetters.append(guess)
-        for x3 in word:
-            if x3==guess:
+        for wordCheker in word:
+            if wordCheker==guess:
                 if guess == word[index]:
-                    correctguesses.insert(0,index)
-                    word2.pop(index)
-                    word2.insert(index,guess)
+                    correctGuesses.insert(0,index)
+                    guessedWord.pop(index)
+                    guessedWord.insert(index,guess)
                 
                 else:
                     index += 1
@@ -56,24 +72,23 @@ if x1==('Y'):
             
         
          
-        print( " ".join(word2) )
+        print( " ".join(guessedWord) )
             
             
   
   
               
-        if correctguesses==[]:
-            x2=x2-1
+        if correctGuesses==[]:
+            lifes=lifes-1
             print("Wrong guess, try again.")
-            print("You have",x2,"tries left.")# writing out of the incorrect guess and the amount of attempts left
+            print("You have",lifes,"tries left.")# writing out of the incorrect guess and the amount of attempts left
         else:
-            print:("you lose challanger, please come again") #"losing screen"
+            print:("you lose challanger, please come again")#"losing screen"
 
     print("You win challanger, please come again") #"winning screen"
-    exit()
+
 else:
     print("then please come back when you feel like playing challanger")
-    exit()#if you answer n you come here 
         
             
             
